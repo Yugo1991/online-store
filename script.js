ea365d5209c4c371c3669cfbb036c374f1590e6d
Yugo1991 <stetneygreene@yahoo.com>
2025-05-23 19:17:53 -0400

    changes

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
 const header = document.querySelector("header");
-const hamburger = document.querySelector(".hamburger");
-hamburger.addEventListener("click", function() {
-    header.classList.toggle("active");
-    hamburger.classList.toggle("active");
-});
 
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
 
 window.addEventListener ("scroll", function(){
     header.classList.toggle ("sticky", this.window.scrollY > 0);
 })
 
-let menu = document.querySelector('#menu-icon');
 let navmenu = document.querySelector('.navmenu');
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navmenu.classList.toggle('open');
-}
-
-document.querySelector('.hamburger').addEventListener('click', () => {
-  document.querySelector('.menu').classList.toggle('active');
-});
 
 let slideIndex = 0;
 const slides = document.querySelectorAll(".slide");
